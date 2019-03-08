4d974eb6b32a6088e5cef5d14b7245e334bb9049
rahulrai <rahulrai@live.com>
2019-03-08 23:45:54 +1100

    reloaded blob and added post back

--- blog/functions.dev/customrss.js
+++ blog/functions.dev/customrss.js
@@ -1,6 +1,37 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World'
-  });
-};
+import Parser from 'rss-parser';
+import { Feed } from 'feed';
+
+export function handler(event, context, callback) {
+  (async () => {
+    let parser = new Parser();
+    let parsedFeed = await parser.parseURL(
+      'https://thecloudblog.net/post/index.xml'
+    );
+
+    const feed = new Feed({
+      title: parsedFeed.title,
+      description: parsedFeed.description,
+      link: parsedFeed.link,
+      language: 'en',
+      generator: 'The Cloud Blog',
+      author: {
+        name: 'Rahul Rai',
+        email: parsedFeed.email
+      }
+    });
+    parsedFeed.items.shift();
+    parsedFeed.items
+      .filter((value, index) => index < 5)
+      .forEach(item => {
+        feed.addItem({
+          title: item.title,
+          link: item.link,
+          description: item.content,
+          published: new Date(item.isoDate),
+          date: new Date(item.isoDate),
+          guid: item.guid
+        });
+      });
+    callback(null, { statusCode: 200, body: feed.atom1() });
+  })();
+}
